3ef7e8c05372379e766425df08e8e9c470ae13f5
Mateus Silva <mateus.avlis@outlook.com>
2025-09-29 10:26:22 -0300

    parte 1

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+    #validações
+    validates :title, presence: true
+    validates :rating, inclusion: { in: %w(G PG PG-13 R NC-17),
+        message: "%{value} não é um rating válido" }
+end
---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+movies = [
+  {:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
+  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
+  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'}
+]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
